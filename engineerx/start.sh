541230c80eda48fe206050e38520b23bac67f439
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-02-27 13:12:45 +0330

    d

--- engineerx/start.sh
+++ engineerx/start.sh
@@ -1,9 +1,6 @@
 set -xe
+rm -r media/downloads/
 mv downloads/ media/downloads/
-
-
-
-
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
--- engineerx/unittest.sh
+++ engineerx/unittest.sh
@@ -1,3 +1,4 @@
 set -xe
+rm -r media/downloads/
 mv downloads/ media/downloads/
 python manage.py test authentication --noinput
