037fa46a68ecf2ddacc52af52717cc527aac0053
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-03-24 18:10:18 +0430

    d

--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-docker build engineerx/ -t hsndocker/backend:latest
-docker build engineerx/dockerfiles/unittest/ -t hsndocker/backend-unittest:latest
-docker build nginx/ -t hsndocker/backend-nginx:latest
-docker build postgres/ -t hsndocker/backend-postgres:latest
